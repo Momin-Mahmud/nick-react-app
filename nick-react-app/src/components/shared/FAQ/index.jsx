ce18c9ce4f99441a525cd1c3bc9f71c68d5f6e1e
arslan030 <arsalan.ahmed@funavry.com>
2024-08-24 23:06:14 +0500

    some changes

--- nick-react-app/src/components/shared/FAQ/index.jsx
+++ nick-react-app/src/components/shared/FAQ/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchInput from "../../../components/base/SearchInput/SearchInput";
 import FAQCard from "../../../components/base/FAQCard/FAQCard";
+import Button from "../../base/Button/Button";
 
 const Faq = () => {
   const data = [
@@ -40,7 +41,7 @@ const Faq = () => {
     <div>
       {/* Header Section */}
       <div
-        className="flex flex-col items-center justify-center text-white h-[264px] mb-16"
+        className="flex flex-col items-center justify-center text-white h-[264px]"
         style={{
           background: "linear-gradient(135deg, #FC00FF 0%, #00B6DE 100%)",
         }}
@@ -52,11 +53,51 @@ const Faq = () => {
       </div>
 
       {/* FAQ Cards Section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-20 bg=[#F9FAFB]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-20 bg-[#FFFFF] py-20 ">
         {data.map((item, index) => (
-          <FAQCard heading={item.heading} icon={item.icon} body={item.body} />
+          <FAQCard
+            key={index}
+            heading={item.heading}
+            icon={item.icon}
+            body={item.body}
+          />
         ))}
       </div>
+
+      <div className="border-b-2 border-gray-300 mb-20 px-20">
+
+      </div>
+
+      <div className="px-20">
+        <div className="flex justify-between items-center bg-white p-6">
+          <div>
+            <div>Still Have Questions?</div>
+            <div>
+              Still have question in mind? Please get in touch with our support
+              team or write us an email.
+            </div>
+          </div>
+          <span>
+            <Button innerText="Tertiary Action" type="secondary" />
+          </span>
+        </div>
+
+        <div className="text-center">
+          <div>
+            LIKE WHAT YOU SEE?
+          </div>
+
+          <div>
+          Let’s work together
+          </div>
+          If you’re looking for a kick-ass product design agency, we’re here to help!
+          <div>
+            <Button type="primary" innerText="Primary Action"/>
+            <Button type="secondary" innerText="Secondary Action"/>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
