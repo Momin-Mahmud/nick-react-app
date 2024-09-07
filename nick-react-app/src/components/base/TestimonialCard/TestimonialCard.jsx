c8caf1df96692efcb77688b6eeb2bf5314cd1ed3
Waleed <waleed.mukhtar1999@gmail.com>
2024-09-07 15:17:57 +0500

    hover scale

--- nick-react-app/src/components/base/TestimonialCard/TestimonialCard.jsx
+++ nick-react-app/src/components/base/TestimonialCard/TestimonialCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TestimonialCard = ({ img, alt }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md w-60 h-20 flex items-center justify-center flex-shrink-0 transform transition-transform duration-300 hover:scale-105">
+    <div className="bg-white rounded-lg shadow-md w-60 h-20 flex items-center justify-center flex-shrink-0 transform transition-transform duration-300 hover:scale-110">
       {/* Centered Image */}
       <img src={img} alt={alt} className="max-w-full max-h-full object-cover rounded-md" />
     </div>
