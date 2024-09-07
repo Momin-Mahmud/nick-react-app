48b46d277b18a6ae9d547adcdcddacffa1f86074
Waleed <waleed.mukhtar1999@gmail.com>
2024-09-07 15:37:07 +0500

    scrolling animation

--- nick-react-app/src/components/shared/Testimonials/index.jsx
+++ nick-react-app/src/components/shared/Testimonials/index.jsx
@@ -7,31 +7,40 @@ import chubb from "../../../assets/Testimonials/Chubb-Limited-logo1.png";
 import wellsfargo from "../../../assets/Testimonials/wellsfargo1.png";
 import rippling from "../../../assets/Testimonials/rippling-logo1.png";
 import gecko from "../../../assets/Testimonials/gecko.png";
-
 import veriz from "../../../assets/Testimonials/veriz.png";
 
 const Testimonials = () => {
   return (
-    <div className="bg-gray-100 py-12">
-      <div className="font-bold text-3xl">
+    <div className="bg-gray-100 py-12 overflow-hidden">
+      <div className="font-bold text-3xl text-center mb-8">
         Our CoPilots Strategies Have Booked Meetings and Deals With
       </div>
-      <div className="flex items-center justify-center my-8">
-        <div className="flex overflow-x-auto space-x-6 p-4 scrollbar-hide">
-          <TestimonialCard img={meta} />
-          <TestimonialCard img={cognizant} />
-          <TestimonialCard img={wellsfargo} />
-          <TestimonialCard img={rippling} />
-          <TestimonialCard img={gecko} />
-          <TestimonialCard img={veriz} />
-          <TestimonialCard img={rippling} />
-          <TestimonialCard img={meta} />
-          <TestimonialCard img={cognizant} />
-          {/* Add more TestimonialCards as needed */}
+      <div className="relative overflow-hidden">
+        {/* Marquee container */}
+        <div className="animate-marquee">
+          <div className="marquee-inner">
+            {/* Original set of cards */}
+            <div className="flex space-x-6 p-4">
+              <TestimonialCard img={meta} />
+              <TestimonialCard img={cognizant} />
+              <TestimonialCard img={wellsfargo} />
+              <TestimonialCard img={rippling} />
+              <TestimonialCard img={gecko} />
+              <TestimonialCard img={veriz} />
+            </div>
+            {/* Duplicate set of cards for continuous effect */}
+            <div className="flex space-x-6 p-4">
+              <TestimonialCard img={meta} />
+              <TestimonialCard img={cognizant} />
+              <TestimonialCard img={wellsfargo} />
+              <TestimonialCard img={rippling} />
+              <TestimonialCard img={gecko} />
+              <TestimonialCard img={veriz} />
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-8">
         <Button type="primary" innerText="Get 7 Day Free Trial" />
       </div>
     </div>
