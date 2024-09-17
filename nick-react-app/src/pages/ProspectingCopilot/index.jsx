c8cce0b968b209cf551a5a3276cfa04bdabe4d99
SaadBajwa24 <i191974@nu.edu.pk>
2024-09-18 00:03:33 +0500

    Email changes

--- nick-react-app/src/pages/ProspectingCopilot/index.jsx
+++ nick-react-app/src/pages/ProspectingCopilot/index.jsx
@@ -1,52 +1,36 @@
-import Icon from '../../utils/Icon';
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import apiRequest from '../../utils/axios/api-request';
+import Icon from "../../utils/Icon";
+import React from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import apiRequest from "../../utils/axios/api-request";
 
 const ProspectingCopilot = () => {
   const items = [
     {
-      label: 'Can you help me write a prospecting email?'
+      label: "Can you help me write a prospecting email?",
     },
     {
-      label: 'Write a sequence to one of my accounts'
+      label: "Write a sequence to one of my accounts",
     },
     {
-      label: 'Craft a follow-up message for a prospect'
+      label: "Craft a follow-up message for a prospect",
     },
     {
-      label: 'Can you write me a custom LinkedIn message'
-    }
+      label: "Can you write me a custom LinkedIn message",
+    },
   ];
 
   const questions = [
-    'Which job title are you reaching out to?',
-    'Which company are you reaching out to?',
-    'Are there any specific triggers or perfect times to reach out to this prospect? (Provide examples such as a 10-K, blog post, LinkedIn profile, about page, job posting, etc.)'
+    "Which job title are you reaching out to?",
+    "Which company are you reaching out to?",
+    "Are there any specific triggers or perfect times to reach out to this prospect? (Provide examples such as a 10-K, blog post, LinkedIn profile, about page, job posting, etc.)",
   ];
 
   const [questionNumber, setQuestionNumber] = useState(0);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [inputArray, setInputArray] = useState([]);
-  const [emailData, setEmailData] = useState([
-    {
-      subject: 'Lorem ipsum dolor sit amet',
-      body: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet'
-    },
-    {
-      subject: 'Lorem ipsum dolor sit amet',
-      body: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet'
-    },
-    {
-      subject: 'Lorem ipsum dolor sit amet',
-      body: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet'
-    },
-    {
-      subject: 'Lorem ipsum dolor sit amet',
-      body: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet'
-    }
-  ]);
+  const [emailData, setEmailData] = useState([]);
+  const [showEmail, setShowEmail] = useState(false);
 
   useEffect(() => {
     console.log(inputArray);
@@ -54,25 +38,25 @@ const ProspectingCopilot = () => {
 
   useEffect(() => {
     if (questionNumber == 3) {
-      console.log('Question 3 done!');
+      console.log("Question 3 done!");
       handleGptResponse();
     }
   }, [questionNumber]);
 
   const createObjectFromQuestions = (questions, inputArray) => {
     return questions.reduce((acc, question, index) => {
-      acc[question] = inputArray[index] || '';
+      acc[question] = inputArray[index] || "";
       return acc;
     }, {});
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       e.preventDefault();
 
-      if (inputValue.trim() !== '') {
+      if (inputValue.trim() !== "") {
         setInputArray([...inputArray, inputValue]); // Add the current input value to the array
-        setInputValue(''); // Clear the input after saving
+        setInputValue(""); // Clear the input after saving
         setQuestionNumber(questionNumber + 1); // Increment the question number
       }
     }
@@ -80,49 +64,62 @@ const ProspectingCopilot = () => {
 
   const handleGptResponse = async () => {
     const questionsObject = createObjectFromQuestions(questions, inputArray);
-    console.log('The object made is: ', questionsObject);
+    console.log("The object made is: ", questionsObject);
 
-    const { data, error } = await apiRequest('post', '/session/add_session', {
+    const { data, error } = await apiRequest("post", "/session/add_session", {
       data: {
         session: {
           session_id: uuidv4(),
-          session_details: questionsObject
-        }
-      }
+          session_details: questionsObject,
+        },
+      },
     });
-    console.log("Data: ", data.prompts[0]?.response?.emails);
-    console.log("Error: ", error);
+    if (error == null) {
+      console.log("Data: ", data?.data?.prompts[0]?.response?.emails);
+      setEmailData(data?.data?.prompts[0]?.response?.emails);
+      setShowEmail(true);
+    } else {
+      console.error("Error: ", error);
+    }
   };
 
   return (
-    <div className='overflow-scroll max-h-[100vh]'>
-    <div className='flex-col flex w-[85vw] pt-48 gap-8 items-center overflow-scroll justify-end'>
-      <div className='flex-col items-center flex justify-center'>
-        <div className='flex flex-col items-center gap-1 mb-4'>
-          <div>
-            <Icon name={'prospectingLogo'} size='70' className='mx-3' />
+    <div className="overflow-scroll max-h-[100vh]">
+      <div className="flex-col flex w-[85vw] pt-48 gap-8 items-center overflow-scroll justify-end">
+        <div className="flex-col items-center flex justify-center">
+          <div className="flex flex-col items-center gap-1 mb-4">
+            <div>
+              <Icon name={"prospectingLogo"} size="70" className="mx-3" />
+            </div>
+            <div className="font-bold text-2xl">Prospecting CoPilot</div>
+            <div>
+              Analyzes business scenarios and crafts tailored outreach messages
+            </div>
           </div>
-          <div className='font-bold text-2xl'>Prospecting CoPilot</div>
-          <div>Analyzes business scenarios and crafts tailored outreach messages</div>
-        </div>
-        <div
-          style={{ boxShadow: 'rgba(0, 0, 0, 0.15) 0px -50px 20px -40px inset' }}
-          className='p-4 text-sm text-left mt-4 flex flex-col justify-start overflow-y-auto max-h-[30vh] mx-16 items-start gap-2 border-[1px] rounded-xl border-[#E9E9E9]'
-        >
-          <div className='font-semibold text-[#4B5563]'>Emails</div>
-          <div className='flex flex-col gap-4 text-xs mt-2'>
-            {emailData.map((data, index) => (
-              <div className='flex flex-col gap-4' key={index}>
-                <div className='font-semibold flex items-center justify-start gap-2'>
-                  <span>Subject:</span>
-                  {data.subject}
+          {showEmail && (
+            <div
+              style={{
+                boxShadow: "rgba(0, 0, 0, 0.15) 0px -50px 20px -40px inset",
+              }}
+              className="p-4 text-sm text-left mt-4 flex flex-col justify-start overflow-y-auto max-h-[30vh] mx-16 items-start gap-2 border-[1px] rounded-xl border-[#E9E9E9]"
+            >
+              <div className="font-semibold text-[#4B5563]">Emails</div>
+              {
+                <div className="flex flex-col gap-4 text-xs mt-2">
+                  {emailData.map((data, index) => (
+                    <div className="flex flex-col gap-4" key={index}>
+                      <div className="font-semibold flex items-center justify-start gap-2">
+                        <span>Subject:</span>
+                        {data.subject}
+                      </div>
+                      <div>{data.body}</div>
+                    </div>
+                  ))}
                 </div>
-                <div>{data.body}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-        {/* <div>
+              }
+            </div>
+          )}
+          {/* <div>
           <div className='mt-4'>
             <div className='flex items-center justify-center gap-5'>
               {items.map((item, index) => (
@@ -138,51 +135,61 @@ const ProspectingCopilot = () => {
             </div>
           </div>
         </div> */}
-      </div>
-      <div className='w-[40vw] mt-6 mb-6'>
-        <div className='flex items-start mb-6 font-bold'>{questionNumber <= 2 ? questions[questionNumber] : ''}</div>
-        <div className='relative mt-2 rounded-md shadow-lg focus:ring-0 bg-white'>
-          <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
-            <span className='text-gray-500 sm:text-sm'>
-              <Icon name='attachments' size='16' />
-            </span>
-          </div>
-          <input
-            type='text'
-            name='chat'
-            id='chat'
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            className='block w-full rounded-md border-[1px] border-[#E5E7EB] py-1.5 focus:outline-none bg-white pl-10 pr-20 placeholder:text-black sm:text-sm sm:leading-6'
-            placeholder='...'
-            onKeyDown={handleKeyDown}
-          />
-          <div className='absolute inset-y-0 right-0 flex pr-2 mt-1 items-center'>
-            <Icon name='upload' size='20' className='' />
-          </div>
         </div>
-        <div className='mt-4'>CoPilots can make mistakes. Check important information</div>
-        {inputArray.length > 0 && (
-          <div
-            style={{ boxShadow: 'rgba(0, 0, 0, 0.15) 0px -50px 20px -40px inset' }}
-            className='p-4 text-sm text-left mt-4 flex flex-col justify-start items-start gap-2 border-[1px] rounded-xl border-[#E9E9E9]'
-          >
-            <div className='font-semibold text-[#4B5563]'>Submitted</div>
-            <div className='flex flex-col gap-4 text-xs mt-2'>
-              {inputArray.map((item, index) => (
-                <div key={item.id} className='flex items-center gap-2'>
-                  <Icon name={'tickWhite'} size='12' className='p-0.5 rounded-full bg-[#6D28D9]' />
-                  <div className='flex flex-col gap-1'>
-                    <div className='font-bold'>{questions[index]}</div>
-                    <div>{item}</div>
-                  </div>
-                </div>
-              ))}
+        <div className="w-[40vw] mt-6 mb-6">
+          <div className="flex items-start mb-6 font-bold">
+            {questionNumber <= 2 ? questions[questionNumber] : ""}
+          </div>
+          <div className="relative mt-2 rounded-md shadow-lg focus:ring-0 bg-white">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="text-gray-500 sm:text-sm">
+                <Icon name="attachments" size="16" />
+              </span>
+            </div>
+            <input
+              type="text"
+              name="chat"
+              id="chat"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              className="block w-full rounded-md border-[1px] border-[#E5E7EB] py-1.5 focus:outline-none bg-white pl-10 pr-20 placeholder:text-black sm:text-sm sm:leading-6"
+              placeholder="..."
+              onKeyDown={handleKeyDown}
+            />
+            <div className="absolute inset-y-0 right-0 flex pr-2 mt-1 items-center">
+              <Icon name="upload" size="20" className="" />
             </div>
           </div>
-        )}
+          <div className="mt-4">
+            CoPilots can make mistakes. Check important information
+          </div>
+          {inputArray.length > 0 && (
+            <div
+              style={{
+                boxShadow: "rgba(0, 0, 0, 0.15) 0px -50px 20px -40px inset",
+              }}
+              className="p-4 text-sm text-left mt-4 flex flex-col justify-start items-start gap-2 border-[1px] rounded-xl border-[#E9E9E9]"
+            >
+              <div className="font-semibold text-[#4B5563]">Submitted</div>
+              <div className="flex flex-col gap-4 text-xs mt-2">
+                {inputArray.map((item, index) => (
+                  <div key={item.id} className="flex items-center gap-2">
+                    <Icon
+                      name={"tickWhite"}
+                      size="12"
+                      className="p-0.5 rounded-full bg-[#6D28D9]"
+                    />
+                    <div className="flex flex-col gap-1">
+                      <div className="font-bold">{questions[index]}</div>
+                      <div>{item}</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
