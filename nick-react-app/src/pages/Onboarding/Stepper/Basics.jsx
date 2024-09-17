89e4290ed8db991bb3023b943a9b39df13b49c2b
SaadBajwa24 <i191974@nu.edu.pk>
2024-09-17 17:45:16 +0500

    Added dropdown in role and company size

--- nick-react-app/src/pages/Onboarding/Stepper/Basics.jsx
+++ nick-react-app/src/pages/Onboarding/Stepper/Basics.jsx
@@ -1,9 +1,25 @@
 const Basics = ({ formData, setFormData }) => {
-    return (
+  const roles = [
+    "COO",
+    "CFO",
+    "CTO",
+    "Director",
+    "Manager",
+    "Partner",
+    "Sales",
+    "Other",
+  ];
+  const companySize = ["0-9", "10-49", "50-249", "250-999", "1000+"];
+
+  return (
     <div className="w-full bg-white p-16 justify-start items-start rounded-lg flex flex-col gap-6">
-     <div className='text-3xl font-bold text-left'>Lets start with the Basics</div>
-      <div className='flex flex-col items-start justify-start'>
-        <label className='block mb-1 text-sm text-slate-800 font-semibold'>Your Name</label>
+      <div className="text-3xl font-bold text-left">
+        Lets start with the Basics
+      </div>
+      <div className="flex flex-col items-start justify-start w-full">
+        <label className="block mb-1 text-sm text-slate-800 font-semibold">
+          Your Name
+        </label>
         <input
           type="text"
           className="w-full h-10 bg-white placeholder:text-xs placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 mb-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
@@ -12,7 +28,7 @@ const Basics = ({ formData, setFormData }) => {
           onChange={(e) => setFormData({ ...formData, name: e.target.value })}
         />
       </div>
-      <div className="flex flex-col items-start justify-start">
+      <div className="flex flex-col items-start justify-start w-full">
         <label className="block mb-1 text-sm text-slate-800 font-semibold">
           Company Name
         </label>
@@ -29,19 +45,49 @@ const Basics = ({ formData, setFormData }) => {
           <span className="text-sm text-[#6B7280]">e.g., Acme Corp.</span>
         </div>
       </div>
-      <div className="flex flex-col items-start justify-start">
+      {/*Roles dropdown */}
+      <div className="flex flex-col items-start justify-start w-full">
         <label className="block mb-1 text-sm text-slate-800 font-semibold">
           Your Role
         </label>
-        <input
-          type="text"
+        <select
           className="w-full h-10 bg-white placeholder:text-xs placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 mb-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
-          placeholder={`Select One`}
           value={formData.role}
           onChange={(e) => setFormData({ ...formData, role: e.target.value })}
-        />
+        >
+          <option value="" disabled selected>
+            Select One
+          </option>
+          {roles.map((role, index) => (
+            <option key={index} value={role}>
+              {role}
+            </option>
+          ))}
+        </select>
       </div>
-      <div className="flex flex-col items-start justify-start">
+      {/* Company size dropdown */}
+      <div className="flex flex-col items-start justify-start w-full">
+        <label className="block mb-1 text-sm text-slate-800 font-semibold">
+          Your Company Size
+        </label>
+        <select
+          className="w-full h-10 bg-white placeholder:text-xs placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 mb-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+          value={formData.companySize}
+          onChange={(e) =>
+            setFormData({ ...formData, companySize: e.target.value })
+          }
+        >
+          <option value="" disabled selected>
+            Placeholder
+          </option>
+          {companySize.map((role, index) => (
+            <option key={index} value={role}>
+              {role}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* <div className="flex flex-col items-start justify-start w-full">
         <label className="block mb-1 text-sm text-slate-800 font-semibold">
           Your Company Size
         </label>
@@ -54,7 +100,7 @@ const Basics = ({ formData, setFormData }) => {
             setFormData({ ...formData, companySize: e.target.value })
           }
         />
-      </div>
+      </div> */}
     </div>
   );
 };
