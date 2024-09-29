840286cd5bbb13a3a321c2599a5b6c8ae850b109
SaadBajwa24 <i191974@nu.edu.pk>
2024-09-30 00:37:52 +0500

    Momin changes

--- nick-react-app/src/components/base/PricingCard/PaymentSuccessful/index.tsx
+++ nick-react-app/src/components/base/PricingCard/PaymentSuccessful/index.tsx
@@ -1,35 +1,45 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import apiRequest from '../../../../utils/axios/api-request'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import apiRequest from "../../../../utils/axios/api-request";
 
 const PaymentSuccessful = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
   const subsribeSuccesss = async () => {
-      const { data, error } = await apiRequest('post', `payment/subscribe`, {
-          data: { detail: {} }
-    })
-  }
+    const { data, error } = await apiRequest("post", `payment/subscribe`, {
+      data: { id: Number(localStorage.getItem("packageId")) },
+    });
+    if (!error) localStorage.removeItem("packageId");
+  };
+
   useEffect(() => {
-    subsribeSuccesss()
-  }, [])
+    subsribeSuccesss();
+  }, []);
 
   return (
-    <div className='bg-white min-h-screen'>
-      <div className='p-24  md:mx-auto'>
-        <svg viewBox='0 0 24 24' className='text-[#86198F] w-16 h-16 mx-auto my-6'>
+    <div className="bg-white min-h-screen">
+      <div className="p-24  md:mx-auto">
+        <svg
+          viewBox="0 0 24 24"
+          className="text-[#86198F] w-16 h-16 mx-auto my-6"
+        >
           <path
-            fill='currentColor'
-            d='M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z'
+            fill="currentColor"
+            d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
           ></path>
         </svg>
-        <div className='text-center'>
-          <h3 className='md:text-2xl text-base text-gray-900 font-semibold text-center'>Payment Done!</h3>
-          <p className='text-gray-600 my-2'>Thank you for completing your secure online payment.</p>
+        <div className="text-center">
+          <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">
+            Payment Done!
+          </h3>
+          <p className="text-gray-600 my-2">
+            Thank you for completing your secure online payment.
+          </p>
           <p> Have a great day! </p>
-          <div className='py-10 text-center'>
+          <div className="py-10 text-center">
             <span
-              onClick={() => navigate('/pricing')}
-              className='px-12 bg-[#86198F] cursor-pointer rounded-md text-white font-semibold py-3'
+              onClick={() => navigate("/pricing")}
+              className="px-12 bg-[#86198F] cursor-pointer rounded-md text-white font-semibold py-3"
             >
               GO BACK
             </span>
@@ -37,7 +47,7 @@ const PaymentSuccessful = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PaymentSuccessful
+export default PaymentSuccessful;
