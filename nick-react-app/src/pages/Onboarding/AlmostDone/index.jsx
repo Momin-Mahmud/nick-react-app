ccb7f2357b48381927a80dbb9e1d249b23b4fa1a
SaadBajwa24 <i191974@nu.edu.pk>
2024-10-05 15:20:15 +0500

    Added new pages in onboarding section

--- nick-react-app/src/pages/Onboarding/AlmostDone/index.jsx
+++ nick-react-app/src/pages/Onboarding/AlmostDone/index.jsx
@@ -1,28 +1,33 @@
-const AlmostDone = ({ formData, setFormData }) => {
+const AlmostDone = ({
+  formData,
+  setFormData,
+  title,
+  question,
+  description,
+  helperText,
+  fieldKey,
+}) => {
   return (
     <div className="w-[30vw] bg-white p-12 pb-52 justify-start items-start rounded-lg flex flex-col gap-2">
       <label className="block mb-1 text-3xl text-slate-800 font-bold">
-        Almost Done
+        {title}
       </label>
       <label className="block mb-1 text-sm text-slate-800 font-semibold">
-        What product or service are you selling?
+        {question}
       </label>
       <textarea
         rows={5}
         className="w-full bg-white placeholder:text-xs placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 mb-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
-        placeholder={`Write a description of what your company sells...`}
-        value={formData.productsOrServicesSelling}
+        placeholder={description}
+        value={formData[fieldKey]}
         onChange={(e) =>
           setFormData({
             ...formData,
-            productsOrServicesSelling: e.target.value,
+            [fieldKey]: e.target.value,
           })
         }
       />
-      <span className="text-sm text-[#6B7280]">
-        e.g., Could based software, financial consulting services. The more
-        detailed your are the more efficient your copilot will be.
-      </span>
+      <span className="text-sm text-[#6B7280]">{helperText}</span>
     </div>
   );
 };
